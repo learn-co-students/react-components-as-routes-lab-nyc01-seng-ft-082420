7434ec7b07a041baffef7466049941b5b7d25a19
AB <austinbuhler@gmail.com>
2020-11-03 13:50:35 -0500

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,9 +2,24 @@ import React from 'react';
 import { actors } from '../data';
 
 const Actors = () => {
+
+  const renderActors = () => {
+    return actors.map((actor, index) => {
+      return (
+        <div key={index}>
+          <h2>{actor.name}</h2>
+          <ul>
+            {actor.movies.map((movie, index) => <li key={index}>{movie}</li>)}
+          </ul>
+        </div>
+      )
+    })
+  }
+
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {renderActors()}
     </div>
   );
 };
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,24 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+
+  const renderMovies = () => {
+    return movies.map((movie, index) => {
+      return(
+        <div key={index}>
+          <h2>{movie.title}</h2>
+          <h4>{movie.time}</h4>
+          <ul>
+            {movie.genres.map((genre, index) => <li key={index}>{genre}</li>)}
+          </ul>
+        </div>
+      )
+    })
+  }
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {renderMovies()}
     </div>
   );
 };
